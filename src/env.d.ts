cb5b7346ec42c72ba67b9cc46a18849726323303
billyhar <billis.h@hotmail.com>
2025-04-27 10:46:05 +0100

    back to identity

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
 
 interface Window {
+  netlifyIdentity: {
+    on: (event: string, callback: (user?: any) => void) => void;
+  }
+}
